5fc3694f953b61cb0e2c95af344becb1b6ec4230
red-eyed-99 <mgnblack99@gmail.com>
2024-12-10 19:18:19 +0300

    feat: add web configuration

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("controllers")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public WebConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+}
