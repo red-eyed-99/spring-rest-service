3d84feee0e65cdb093fdb5bbb3930cb6a66ea52d
red-eyed-99 <mgnblack99@gmail.com>
2024-12-12 13:42:34 +0300

    feat: add custom default pretty printer for a more beautiful json response display

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,11 +1,20 @@
 package config;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import utils.JsonPrettyPrinter;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan("controllers")
@@ -17,5 +26,25 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     public WebConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public DefaultPrettyPrinter defaultPrettyPrinter() {
+        return new JsonPrettyPrinter();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.setDefaultPrettyPrinter(defaultPrettyPrinter());
+
+        return objectMapper;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper()));
     }
 }
---
+++ src/main/java/utils/JsonPrettyPrinter.java
@@ -0,0 +1,24 @@
+package utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+
+import java.io.IOException;
+
+public class JsonPrettyPrinter extends DefaultPrettyPrinter {
+
+    @Override
+    public DefaultPrettyPrinter createInstance() {
+        var jsonPrettyPrinter = new JsonPrettyPrinter();
+
+        jsonPrettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+
+        return jsonPrettyPrinter;
+    }
+
+    @Override
+    public void writeObjectFieldValueSeparator(JsonGenerator jsonGenerator) throws IOException {
+        jsonGenerator.writeRaw(": ");
+    }
+}
