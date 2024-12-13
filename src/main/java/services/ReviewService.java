fc1ff9c7a2cd4f3c39c6874db15cc5c46c5390c0
red-eyed-99 <mgnblack99@gmail.com>
2024-12-13 17:41:30 +0300

    feat: improve the review service

--- src/main/java/services/ReviewService.java
+++ src/main/java/services/ReviewService.java
@@ -4,29 +4,52 @@ import dto.review.BookReviewDTO;
 import dto.review.CreateReviewDTO;
 import dto.review.ReviewResponseDTO;
 import mappers.ReviewMapper;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import repositories.BookRepository;
+import repositories.ReaderRepository;
 import repositories.ReviewRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
+    private final ReaderRepository readerRepository;
 
     private final ReviewMapper reviewMapper;
 
     @Autowired
-    public ReviewService(ReviewRepository reviewRepository, ReviewMapper reviewMapper) {
+    public ReviewService(ReviewRepository reviewRepository, BookRepository bookRepository,
+                         ReaderRepository readerRepository, ReviewMapper reviewMapper) {
+
         this.reviewRepository = reviewRepository;
+        this.bookRepository = bookRepository;
+        this.readerRepository = readerRepository;
+
         this.reviewMapper = reviewMapper;
     }
 
     @Transactional
     public ReviewResponseDTO create(CreateReviewDTO createReviewDTO) {
+        var book = bookRepository
+                .findById(createReviewDTO.getBookId())
+                .orElseThrow();
+
+        Hibernate.initialize(book.getAuthors());
+
+        var reader = readerRepository
+                .findById(createReviewDTO.getReaderId())
+                .orElseThrow();
+
         var review = reviewMapper.toReview(createReviewDTO);
+        review.setBook(book);
+        review.setReader(reader);
 
         reviewRepository.create(review);
 
@@ -38,7 +61,7 @@ public class ReviewService {
         var reviews = reviewRepository.findAll(bookId);
 
         if (reviews.isEmpty()) {
-            throw new RuntimeException("No reviews found");
+            throw new NoSuchElementException("No reviews found");
         }
 
         return reviewMapper.toBookReviewDTOList(reviews);
