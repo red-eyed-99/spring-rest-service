5c4c36134ccbd1e9628279e42705467e0e499f26
red-eyed-99 <mgnblack99@gmail.com>
2024-12-15 19:14:32 +0300

    feat: delete unused annotations

--- src/main/java/dto/author/UpdateAuthorNameDTO.java
+++ src/main/java/dto/author/UpdateAuthorNameDTO.java
@@ -1,25 +1,13 @@
 package dto.author;
 
-import jakarta.validation.constraints.*;
-
-import java.beans.ConstructorProperties;
-
 public class UpdateAuthorNameDTO {
 
-    @NotNull(message = "Missing author 'id' parameter")
-    @Digits(integer = 19, fraction = 0, message = "Incorrect author 'id' parameter")
-    @Positive(message = "Author 'id' must be a positive number greater than zero")
     private final Long id;
 
-    @NotBlank(message = "Missing author 'first-name' parameter")
-    @Size(max = 30, message = "Author 'first-name' must be no more than 30 characters")
     private final String firstName;
 
-    @NotBlank(message = "Missing author 'last-name' parameter")
-    @Size(max = 30, message = "Author 'last-name' must be no more than 30 characters")
     private final String lastName;
 
-    @ConstructorProperties(value = {"id", "first-name", "last-name"})
     public UpdateAuthorNameDTO(Long id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
--- src/main/java/dto/review/CreateReviewDTO.java
+++ src/main/java/dto/review/CreateReviewDTO.java
@@ -1,26 +1,13 @@
 package dto.review;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-
 import java.time.LocalDate;
 
 public class CreateReviewDTO {
 
-    @NotBlank(message = "Missing reader 'id' parameter")
-    @Digits(integer = 19, fraction = 0, message = "Incorrect reader 'id' parameter")
-    @Positive(message = "Reader 'id' must be a positive number greater than zero")
     private final Long readerId;
 
-    @NotBlank(message = "Missing book 'id' parameter")
-    @Digits(integer = 19, fraction = 0, message = "Incorrect book 'id' parameter")
-    @Positive(message = "Book 'id' must be a positive number greater than zero")
     private final Long bookId;
 
-    @NotBlank(message = "Missing review 'content' parameter")
-    @Size(max = 255, message = "Review 'content' parameter must be no more than 500 characters")
     private final String content;
 
     private final LocalDate date;
